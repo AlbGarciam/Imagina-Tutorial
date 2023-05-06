cbfe016da25d6f463f2da41ee5254336e7b5c344
AlbGarciam <alb.garciam@gmail.com>
2023-05-06 11:32:51 +0200

    Chapter 1 - Configure project

---
+++ LoginSample/LoginSample/LoginSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginSampleApp.swift
+//  LoginSample
+//
+//  Created by Alberto García-Muñoz on 6/5/23.
+//
+
+import SwiftUI
+
+@main
+struct LoginSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
